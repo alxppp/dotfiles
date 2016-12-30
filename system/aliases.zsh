4d34f5c6c4daaf4b7709d034db9bcd5ab255266a
Alexander Popp <alxppp@googlemail.com>
2016-12-30 12:59:42 +0100

    Swap l and ll

--- system/aliases.zsh
+++ system/aliases.zsh
@@ -4,7 +4,7 @@
 if $(gls &>/dev/null)
 then
   alias ls="gls -F --color"
-  alias l="gls -lAh --color"
-  alias ll="gls -l --color"
+  alias l="gls -l --color"
+  alias ll="gls -lAh --color"
   alias la='gls -A --color'
 fi
