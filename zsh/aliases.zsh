bfcdd6142973a1b2918adac3a79b38d48b3c51e4
Alexander Popp <alxppp@googlemail.com>
2017-01-27 22:20:49 +0100

    Add show/hide desktop aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,3 +1,7 @@
 alias reload!='. ~/.zshrc'
 
 alias cls='clear' # Good 'ol Clear Screen command
+
+# Hide/show all desktop icons (useful when presenting)
+alias hidedesktop="defaults write com.apple.finder CreateDesktop -bool false && killall Finder"
+alias showdesktop="defaults write com.apple.finder CreateDesktop -bool true && killall Finder"
