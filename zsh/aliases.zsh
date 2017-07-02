c1cbedc96fcb3820561ec2d270ecdd54754ab962
Alexander Popp <alxppp@googlemail.com>
2017-07-02 12:53:47 +0200

    Add webserver alias

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -5,3 +5,5 @@ alias cls='clear' # Good 'ol Clear Screen command
 # Hide/show all desktop icons (useful when presenting)
 alias hidedesktop="defaults write com.apple.finder CreateDesktop -bool false && killall Finder"
 alias showdesktop="defaults write com.apple.finder CreateDesktop -bool true && killall Finder"
+
+alias webserver="python -m SimpleHTTPServer 8000"
