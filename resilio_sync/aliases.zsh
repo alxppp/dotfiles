71c833d1802c8728b44d380b7544fc5a5561088a
Alexander Popp <alxppp@googlemail.com>
2017-08-28 12:23:38 +0200

    Add Resilio Sync aliases

---
+++ resilio_sync/aliases.zsh
@@ -0,0 +1,2 @@
+alias lsselectivesync='find . \( -name "*.rsls" -o -name "*.rslsd" -o -name "*.rslsc" -o -name "*.rslsi" -o -name "*.rslss" -o -name "*.rslsp" -o -name "*.rslsf" -o -name "*.rslsz" \)'
+alias rmselectivesync='find . \( -name "*.rsls" -o -name "*.rslsd" -o -name "*.rslsc" -o -name "*.rslsi" -o -name "*.rslss" -o -name "*.rslsp" -o -name "*.rslsf" -o -name "*.rslsz" \) -delete'
