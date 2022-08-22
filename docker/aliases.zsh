0dd4c852cc72f7533fb17ec5aff1504231c806f5
Alexander Popp <alxppp@googlemail.com>
2022-08-22 17:48:29 +0200

    Update Docker aliases

--- docker/aliases.zsh
+++ docker/aliases.zsh
@@ -1,7 +1,4 @@
 alias d='docker $*'
-alias d-c='LOCALIP=$(localip) docker-compose $*'
-alias d-cp='LOCALIP=$(localip) docker-compose -f docker-compose.yml -f docker-compose.prod.yml $*'
-alias dap='d-c exec app $*'
+alias d-c='docker-compose $*'
 alias dps='docker ps -a --format="table {{.ID}}\t{{.Status}}\t{{.Ports}}\t{{.Names}}"'
-alias dclock='/usr/local/bin/docker run --rm --privileged alpine hwclock -s'
 alias ddown='f() { docker ps -aq --filter="name=$1" | xargs docker stop | xargs docker rm };f'
